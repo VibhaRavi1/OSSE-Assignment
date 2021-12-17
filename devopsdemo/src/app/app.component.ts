6887caedf2c53c407f8d8419db0828aa197ff379
Vibha R <vibha.r@sap.com>
2021-12-17 14:37:23 +0530

    Changing get message functionality in front end

--- devopsdemo/src/app/app.component.ts
+++ devopsdemo/src/app/app.component.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
- 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
-  url: string = 'http://localhost:8080/webapi/messages';
+
+  url: string = 'http://localhost:5000/webapi/messages';
   msg: string = '';
   fetched_msg:any = '';
   send_msg()
   {
     let data = {
-      "author": "Ananddddd",
+      "author": "Vibha",
       "created": "2021-05-14T07:33:19.084Z[UTC]",
       "id": 3
      };
@@ -24,18 +24,19 @@ export class AppComponent {
     alert("Data Sent");
     });
   }
- 
+
   async get_msg()
   {
-    let temp_data = await this.http.get(this.url).toPromise().then();
+    let temp_data: any = await this.http.get(this.url).toPromise().then();
     // this.fetched_msg = temp_data.valueOf();
-    let n = Object.keys(temp_data).length;
-    let temp_data1 = await this.http.get(this.url+"/"+n).toPromise().then();
-    this.fetched_msg = temp_data1;
-    console.log(temp_data1);
+    // let n = Object.keys(temp_data).length;
+    // let temp_data1 = await this.http.get(this.url+"/"+n).toPromise().then();
+    // this.fetched_msg = temp_data1;
     console.log(temp_data);
-    
+    alert(temp_data.message)
+    //console.log(temp_data.message);
+
   }
- 
+
   constructor(private http: HttpClient){}
 }
